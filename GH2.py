8957acd7ee1a5f9666008d63bf4cd46c5a56d91b
Kestutis <kurkestutis@gmail.com>
2019-05-05 00:56:25 +0300

    Tipo jei nėra komento tai tada neuplaudinat?

--- GH2.py
+++ GH2.py
@@ -1,3 +1,12 @@
 #FAILAS TESTUI
 
 print("va kazka atprintinsim")
+
+"""
+pridedu čia dar eilucius
+"""
+
+x = 0
+while x < 10:
+    print("x yra mažiau už dešimt nes jis yra lygus " + str(x))
+    x += 1
