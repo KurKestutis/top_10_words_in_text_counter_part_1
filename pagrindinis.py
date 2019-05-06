c870d18f219d28f2006f2bc50dfa6b72fbfe67f2
Kestutis <kurkestutis@gmail.com>
2019-05-07 01:10:47 +0300

    Veikia get() per lambda

--- pagrindinis.py
+++ pagrindinis.py
@@ -3,22 +3,26 @@ from paste5 import *
 langas = Tk()
 langas.title("Dažniausiai tekste panaudotų žodžių skaičiuoklė (Top - 10) KK X 4.0")
 langas.iconbitmap(r'top_X4.ico')
+global duomenys
 
 # def Pasiimti duomenis iš bokso
-def pasiimti(event):
-    print(boksas.get())
-    duomenys = boksas.get()
+def pasiimti():
+    global duomenys
+    duomenys = boksas.get("1.0", "end-1c")
     print("Štai kas paiimta iš bokso: " + str(duomenys))
+
 
 """-----------Laukai/Mygtukai/Užrašai----------"""
 
-# boksas = Text(langas, height=20, width=70)
-boksas = Entry(langas)
-
+boksas = Text(langas, height=20, width=70)
 
 uzrasas1 = Label(langas, text="Įveskite tekstą:")
-mygtukas_pateikti = Button(langas, text="          Pateikti          ")
-mygtukas_pateikti.bind("<Button-1>", pasiimti)
+mygtukas_pateikti = Button(langas, text="          Pateikti          ", command=lambda: pasiimti())
+#command=lambda: pasiimti() >>> just means do this when i press the button
+
+
+# mygtukas_pateikti.bind("<Button-1>", pasiimti)
+# mygtukas_pateikti=Button(langas, command=lambda: pasiimti())
 # mygtukas1.bind("<Button-1>", spausdinti) PRISIMINIMUI
 # laukas1.bind("<Return>", spausdinti)
 
@@ -57,7 +61,6 @@ boksas.bind('<Button-3>', rClicker, add='')
 # row=2, column=1, columnspan=6, sticky=W
 
 # scroll.config(command=)
-
 
 
 langas.mainloop()
